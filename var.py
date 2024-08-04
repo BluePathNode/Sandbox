7f08372fdf6b2a983b5a1825bd9d8885c7d7b304
Blue <astapp82@gmail.com>
2024-08-04 05:37:04 -0400

    First File

---
+++ var.py
@@ -0,0 +1,24 @@
+
+name = "Bob"
+print(name)
+
+age = 25 
+print(age)
+
+height = 5.9
+print(height)
+
+is_student = True
+print(is_student)
+
+user_name = "Bob94"
+print(user_name)
+
+
+
+
+
+
+
+
+
