74b2d161698213a318eefbbaecd201e273463050
Blue <astapp82@gmail.com>
2024-08-05 09:45:07 -0400

    playing with strings

--- data_types.py
+++ data_types.py
@@ -126,3 +126,61 @@ print("Lightsaber".ljust(24, ".") + "Blue".rjust(6))
 print("Accuracy".ljust(26, ".") + amount.rjust(4))
 
 
+# string index values
+print(first[1])
+print(first[-1])
+print(first[1:-1])
+print(first[1:])
+
+# Some methods return boolean data
+print(first.startswith("D"))
+print(first.endswith("Z"))
+
+
+# Boolean data type
+myvalue = True
+x = bool(False)
+print(type(x))
+print(isinstance(myvalue, bool))
+
+# Numeric data types
+
+# integer type
+price = 100
+best_price = int(80)
+print(type(price))
+print(isinstance(best_price, int))
+
+# float type
+gpa = 3.28
+y = float(1.14)
+print(type(gpa))
+
+# complex type
+comp_value = 5+3j
+print(type(comp_value))
+print(comp_value.real)
+print(comp_value.imag)
+
+# Built-in functions for numbers
+
+print(abs(gpa))
+print(abs(gpa * -1))
+
+print(round(gpa))
+
+print(round(gpa, 1))
+
+
+print(math.pi)
+print(math.sqrt(64))
+print(math.ceil(gpa))
+print(math.floor(gpa))
+
+# Casting a string to a number
+zipcode = "10001"
+zip_value = int(zipcode)
+print(type(zip_value))
+
+# Error if you attempt to cast incorrect data
+# zip_value = int("New York")
